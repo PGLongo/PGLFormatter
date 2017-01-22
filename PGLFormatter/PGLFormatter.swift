66372b7e075b006c43f5e682613706b24d04409a
PGLongo <piergiuseppe.longo@gmail.com>
2017-01-22 16:43:01 +0100

    Update PGLFormatter header

--- PGLFormatter/PGLFormatter.swift
+++ PGLFormatter/PGLFormatter.swift
@@ -1,9 +1,9 @@
 //
-//  ExSwiftFormatter.swift
-//  ExSwift
+//  PGLFormatter.swift
+//  PGLFormatter
 //
 //  Created by Piergiuseppe Longo on 27/02/15.
-//  Copyright (c) 2015 pNre. All rights reserved.
+//  Copyright (c) 2015 Piergiuseppe Longo. All rights reserved.
 //
 
 import Foundation
