c1437a87c46e1b1e6635177efbe58fc9bc987dba
PGLongo <piergiuseppe.longo@gmail.com>
2017-01-22 16:55:43 +0100

    Update header and documentation

--- PGLFormatter/NSNumberFormatter.swift
+++ PGLFormatter/NSNumberFormatter.swift
@@ -1,19 +1,18 @@
 //
 //  NSNumberFormatter.swift
-//  ExSwift
+//  PGLFormatter
 //
-//  Created by Piergiuseppe Longo on 07/03/15.
-//  Copyright (c) 2015 pNre. All rights reserved.
+//  Created by Piergiuseppe Longo on 27/02/15.
+//  Copyright (c) 2015 Piergiuseppe Longo. All rights reserved.
 //
 
 import Foundation
 
 public extension NumberFormatter {
 
-    /**
-        Set the number of digit
-    */
-
+    /// Set the number of digit
+    ///
+    /// - Parameter precision: set the minimum and the maximum fraction digits to precision
     func setPrecision(_ precision: Int) {
         self.minimumFractionDigits = precision
         self.maximumFractionDigits = precision
